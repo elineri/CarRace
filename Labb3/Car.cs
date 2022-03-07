b2be561ecbd767988342078c2ae5d1dd66963851
elineri <elin.ericstam@gmail.com>
2022-03-07 10:57:19 +0100

    Add project files.

---
+++ Labb3/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb3
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Labb3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Labb3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car() { Id = 1, Name = "Snail" };
+            Car c2 = new Car() { Id = 1, Name = "Lightning" };
+        }
+    }
+}
